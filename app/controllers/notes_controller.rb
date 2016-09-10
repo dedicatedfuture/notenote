9eade7e3cd0d676bc992f70b99b624c681ae231e
William Zerega <Bill@Williams-MacBook-Pro-7.local>
2016-09-10 10:59:53 -0400

    added link between user and notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,4 @@
-class NotesController < ApplicationController
+ class NotesController < ApplicationController
 	before_action :find_note, only: [:show, :edit, :update, :destroy]
 	def index
 		@notes = Note.all.order("created_at DESC")
@@ -8,11 +8,11 @@ class NotesController < ApplicationController
 	end
 
 	def new
-		@note = Note.new
+		@note = current_user.notes.build
 	end
 
 	def create
-		@note = Note.new(note_params)
+		@note = current_user.notes.build(note_params)
 		if @note.save 
 			redirect_to @note
 		else
